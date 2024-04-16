0079eeca1745973bb1b208aadc7f418541e36fae
KonstantinovArtur <akonstantinov538@gmail.com>
2024-04-16 15:30:25 +0300

    Добавьте файлы проекта.

---
+++ pr_3/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using pr_3.Practika2DataSetTableAdapters;
+
+namespace pr_3
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Practika2Entities3 context = new Practika2Entities3();
+        List<Table> Tables = new List<Table>();
+        District2TableAdapter district = new District2TableAdapter();   
+        public MainWindow()
+        {
+            InitializeComponent();
+            Dtg2.ItemsSource = district.GetFullInfo();
+            Dtg.ItemsSource = context.District2.ToList();
+            foreach (var item in context.District2.ToList())
+            {
+                Tables.Add(new Table(item));
+            }
+            
+        }
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Dtg2.Columns[0].Visibility = Visibility.Collapsed;
+            Dtg2.Columns[2].Visibility = Visibility.Collapsed;
+
+        }
+    }
+}
---
+++ pr_3/Table.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pr_3
+{
+    internal class Table
+    {
+        public int HouseID;
+        public int DistrictID;
+        public string HouseName { get; private set; }
+        public string DistrictName { get; private set; }
+        public Table(District2 hou)
+        {
+            DistrictID = hou.ID;
+            DistrictName = hou.Name1;
+            if (hou. houses2 != null)
+            {
+                HouseID = hou.houses2.ID;
+                HouseName = hou.houses2.Name1;
+            }
+
+        }
+    }
+}
